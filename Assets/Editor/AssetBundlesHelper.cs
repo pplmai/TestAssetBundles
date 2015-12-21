681dd699634ad340683a01b458845fa3248493b1
Thanaphat Tassanavijitwong <mai@bbdt.com>
2015-12-21 19:59:37 +0700

    update ignore file

--- Assets/Editor/AssetBundlesHelper.cs
+++ Assets/Editor/AssetBundlesHelper.cs
@@ -6,7 +6,7 @@ public class CreateAssetBundles
 	[MenuItem ("Assets/Build AssetBundles")]
 	static void BuildAllAssetBundles ()
 	{
-		BuildPipeline.BuildAssetBundles ("AssetBundles");
+		BuildPipeline.BuildAssetBundles ("AssetBundlesOutput");
 	}
 }
 
